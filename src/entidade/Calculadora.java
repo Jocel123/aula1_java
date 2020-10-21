934f2561944bb8d0066fcc6daa09038d21f4ce07
Jocel Viana <jocelviana@gmail.com>
2020-10-21 18:08:21 -0200

    projeto criado

---
+++ src/entidade/Calculadora.java
@@ -0,0 +1,20 @@
+package entidade;
+
+public class Calculadora {
+
+	public void somar(int num1, int num2) {
+		System.out.println("A soma é: " + (num1 + num2));
+	}
+
+	public void subtracao(int num1, int num2) {
+		System.out.println("A subtração é: " + (num1 - num2));
+	}
+
+	public void divisao(int num1, int num2) {
+		System.out.println("A divisao é: " + (num1 / num2));
+	}
+
+	public void multiplicacao(int num1, int num2) {
+		System.out.println("A multiplicação é: " + (num1 * num2));
+	}
+}
---
+++ src/programa/CalcTeste.java
@@ -0,0 +1,17 @@
+package programa;
+
+import entidade.Calculadora;
+
+public class CalcTeste {
+
+	public static void main(String[] args) {
+	
+		Calculadora calc = new Calculadora();
+		 calc.somar(2, 4);
+		 calc.divisao(5, 2);
+		 calc.subtracao(7, 3);
+		 calc.multiplicacao(4, 3);
+
+	}
+
+}
